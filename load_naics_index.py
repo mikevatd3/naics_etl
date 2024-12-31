34d267598a53804fd1f82792aaafb3a292d2ccbb
mikevatd3 <mike@datadrivendetroit.org>
2024-12-31 09:24:12 -0500

    First commit.

--- load_naics_index.py
+++ load_naics_index.py
@@ -1,40 +1,124 @@
+"""
+This script loads the NAICS tables to the database -- it also is an 
+example standard transformation and load script.
+
+Key features:
+    - Using a pandera schema to validate the data and to capture metadata.
+    - Using logging to track when the file was loaded.
+
+
+Define Each Field that you'd like to include (Something else will have to 
+be done for ACS loads).
+"""
+
 from pathlib import Path
 import logging
+
+import click
+import pygit2
 import pandas as pd
-import geopandas as gpd
+import pandera as pa
+from pandera.typing import Series
+import tomli
 
-from loader import build_workflow, LoadFileType, StopThePresses
-from naics import config, db_engine, setup_logging
+from naics import setup_logging, db_engine
+from metadata_audit.datatypes import Topic, Table, Edition
+from metadata_audit.git import log_git
 
 
-logger = logging.getLogger(config["app"]["name"])
-setup_logging()
+def record_metadata(
+    schema, 
+    table_name, 
+    metadata,
+    edition_date,
+):
+    schema_dict = schema.to_json_schema()
+    schema_variables = schema_dict["properties"]
 
-
-def cleanup_function(input_file: pd.DataFrame | gpd.GeoDataFrame):
-
-    result = input_file.rename(columns={
-        'Unnamed: 0': "id", 
-        'NAICS22': "naics_2022", 
-        'INDEX ITEM DESCRIPTION': "description"
-    })
+    for variable in metadata["tables"][table_name]["variables"]:
+        variable["data_type"] = schema_variables[variable["name"]]["items"]["type"]
     
-    logger.info(result.head())
+    # Validate base topic -- it skips the recursive check
+    Topic(**metadata)
+
+    # Pydantic Validation of current Table metadata
+    Table(**metadata["tables"][table_name])
+
+    # Pydantic Validation of Edition metadata
+
+    edition = metadata["tables"][table_name]["editions"][edition_date]
+
+    edition["version"] = log_git(Path(__file__).resolve())
     
-    return result
+    Edition(**edition)
 
 
-def main():
-    workflow = build_workflow(
-        config,
-        "naics_detail",
-        Path.cwd(),
-        cleanup_function,
-        db_engine,
-        LoadFileType.CSV,
+class NAICSDescriptions(pa.DataFrameModel):
+    """
+    This is one of two tables that make sense to include
+    """
+
+    code: str = pa.Field(unique=True)
+    title: str = pa.Field()
+    description: str = pa.Field(nullable=True)
+
+    class Config:  # type: ignore
+        strict = True
+        coerce = True
+
+    @pa.check("description")
+    def max_nulls(cls, description: Series[str]) -> bool:
+        """
+        It's okay for some of these to be null, but if there are too many
+        it could indicate a problem.
+        """
+        return description.isna().sum() < 200
+
+
+@click.command()
+@click.argument("edition_date")
+def main(edition_date):
+    table_name = "naics_descriptions"
+
+    with open("config.toml", "rb") as f:
+        config = tomli.load(f)
+
+    logger = logging.getLogger(config["app"]["name"])
+
+    # 'Setup logging' makes sure the logging is saved in the right place.
+    setup_logging()
+
+
+    with open("metadata.toml", "rb") as md:
+        metadata = tomli.load(md)
+    
+    result = (
+        pd.read_csv(
+            Path.cwd().parent.parent
+            / "data"
+            / "naics"
+            / "raw"
+            / "naics_descriptions_2022.csv"
+        )
+        .rename(
+            columns={
+                "Code": "code",
+                "Title": "title",
+                "Description": "description",
+            }
+        )
+        .drop("Unnamed: 0", axis=1)
     )
 
-    workflow()
+    
+    # Validate 
+    validated = NAICSDescriptions.validate(result)
+    record_metadata(NAICSDescriptions, table_name, metadata, edition_date)
+    # Is this an issue? Probably not.
+#     with db_engine.connect() as db:
+#         validated.to_sql(
+#             "naics_codes", db, index=False, schema="naics", if_exists="replace"
+#         )
 
 
 if __name__ == "__main__":
--- preload_naics.py
+++ preload_naics.py
@@ -1,4 +1,4 @@
-from pathlib import Path 
+from pathlib import Path
 import logging
 import pandas as pd
 
@@ -10,11 +10,35 @@ setup_logging()
 
 def main():
     logging.info("Changing over xlsx from the census site to csv")
-    file = pd.read_excel(Path.cwd() / "raw" / "2022_NAICS_Index_File.xlsx")
-    file.to_csv(Path.cwd() / "raw" / "naics_index_file_2022.csv")
+    file = pd.read_excel(
+        Path.cwd().parent.parent
+        / "data"
+        / "naics"
+        / "raw"
+        / "2022_NAICS_Index_File.xlsx"
+    )
+    file.to_csv(
+        Path.cwd().parent.parent
+        / "data"
+        / "naics"
+        / "raw"
+        / "naics_index_file_2022.csv"
+    )
 
-    file = pd.read_excel(Path.cwd() / "raw" / "2022_NAICS_Descriptions.xlsx")
-    file.to_csv(Path.cwd() / "raw" / "naics_descriptions_2022.xlsx")
+    file = pd.read_excel(
+        Path.cwd().parent.parent
+        / "data"
+        / "naics"
+        / "raw"
+        / "2022_NAICS_Descriptions.xlsx"
+    )
+    file.to_csv(
+        Path.cwd().parent.parent
+        / "data"
+        / "naics"
+        / "raw"
+        / "naics_descriptions_2022.csv"
+    )
 
 
 if __name__ == "__main__":
